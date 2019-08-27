1d692da123f7f756a81f38af1fd981ba16321240
CO-NET <CO-NET@CO-NET>
2019-08-27 15:57:36 -0300

    Inicio

---
+++ MiVeterinaria.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MiVeterinaria.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ MiVeterinaria.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MiVeterinaria.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
